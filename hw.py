20cb7b21d34cdb62e94f207ea598875aaea98165
UmaCG <72730429+UmaCG@users.noreply.github.com>
2021-04-18 18:09:11 +0100

    Update hw.py
    Testing file upload

--- hw.py
+++ hw.py
@@ -3,3 +3,4 @@ import sys, math       # load system and math module
 r = float(sys.argv[1]) # extract the 1st command-line argument
 s = math.sin(r)
 print "Hello, World! sin(" + str(r) + ")=" + str(s)
+#######
